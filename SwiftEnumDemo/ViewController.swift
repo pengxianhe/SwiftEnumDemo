320c95a2cdd090adbac7f5070ba163d840f66f61
Maynard <pengxianhe@newborntown.com>
2017-07-17 16:02:33 +0800

    新需求： 分享放到公英制下面

--- SwiftEnumDemo/ViewController.swift
+++ SwiftEnumDemo/ViewController.swift
@@ -19,11 +19,11 @@ class ViewController: UIViewController {
     
     var dataArray: [[SetContent]] = [[SetContent(title: "健康数据", content: "1"),
                                       SetContent(title: "公英制", content: "2"),
+                                      SetContent(title: "分享", content: "6"),
                                       SetContent(title: "语言选择", content: "3"),
                                       SetContent(title: "介绍说明", content: "4")],
                                  [SetContent(title: "训练模式", content: "5")],
-                                 [SetContent(title: "分享", content: "6"),
-                                  SetContent(title: "意见反馈", content: "7"),
+                                 [SetContent(title: "意见反馈", content: "7"),
                                   SetContent(title: "关于", content: "8"),
                                   SetContent(title: "评分", content: "9")]]
     
@@ -60,8 +60,10 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
             case 1:
                 performSegue(withIdentifier: "unitVC", sender: nil)
             case 2:
-                performSegue(withIdentifier: "languageVC", sender: nil)
+                performSegue(withIdentifier: "shareVC", sender: nil)
             case 3:
+                performSegue(withIdentifier: "languageVC", sender: nil)
+            case 4:
                 performSegue(withIdentifier: "introduceVC", sender: nil)
             default:
                 break
@@ -73,27 +75,11 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         case 2:
             switch indexPath.row {
             case 0:
-                performSegue(withIdentifier: "shareVC", sender: nil)
-            case 1:
                 performSegue(withIdentifier: "feedbackVC", sender: nil)
-            case 2:
-                performSegue(withIdentifier: "aboutVC", sender: nil)
-            case 3:
-                performSegue(withIdentifier: "scoreVC", sender: nil)
-            default:
-                break
-            }
-
-        case 3:
-            switch indexPath.row {
-            case 0:
-                performSegue(withIdentifier: "healthDataVC", sender: nil)
             case 1:
-                performSegue(withIdentifier: "healthDataVC", sender: nil)
+                performSegue(withIdentifier: "aboutVC", sender: nil)
             case 2:
-                performSegue(withIdentifier: "healthDataVC", sender: nil)
-            case 3:
-                performSegue(withIdentifier: "healthDataVC", sender: nil)
+                performSegue(withIdentifier: "scoreVC", sender: nil)
             default:
                 break
             }
