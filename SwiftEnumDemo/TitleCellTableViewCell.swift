0f7800eca9c5a8ab833ff2c274c2b76a936f8819
Maynard <pengxianhe@newborntown.com>
2017-07-17 15:24:31 +0800

    创建设置界面

---
+++ SwiftEnumDemo/TitleCellTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TitleCellTableViewCell.swift
+//  SwiftEnumDemo
+//
+//  Created by Maynard on 2017/7/17.
+//  Copyright © 2017年 赤子城. All rights reserved.
+//
+
+import UIKit
+
+class TitleCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SwiftEnumDemo/ViewController.swift
+++ SwiftEnumDemo/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataArray: [[String]] = [["健康数据", "公英制", "语言选择", "介绍说明"],
+                                 ["训练模式"],
+                                 ["分享", "意见反馈", "关于", "评分"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TitleCellTableViewCell") as! TitleCellTableViewCell
+        cell.titleLabel.text = dataArray[indexPath.section][indexPath.row]
+        return cell
+    }
+}
